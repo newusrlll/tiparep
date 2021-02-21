b643e86b2d057cf2acf625042458181bb4eebc8d
newusrlll <lebochkin2@yandex.ru>
2021-02-21 15:23:55 +0300

    Закончил первое задание.

--- quotes_prog.py
+++ quotes_prog.py
@@ -8,7 +8,20 @@ while True:
 data = file.read()
 print(data)
 with open(filename, "a", encoding='utf-8') as file:
+    authors = []
     a = input('Автор фразы?')
+    authors.append(a)
     data = file.write('(' + a + ')\n')
     print(data)
+    while True:
+        b = input('Хотите добавить ещё одну цитату?(да/нет)')
+        if b == 'да':
+            c = input('Введите цитату:')
+            data = file.write(c + '\n')
+            a = input('Автор фразы:')
+            authors.append(a)
+            data = file.write('(' + a + ')\n')
+        else:
+            print(authors)
+            break
 file.close()
