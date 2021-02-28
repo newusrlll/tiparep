3f0de0f5cdf4f2ecd1326b23c7c879ab077f4fc5
newusrlll <lebochkin2@yandex.ru>
2021-02-28 15:29:30 +0300

    Первая часть второго задания

--- tiparep2/pupils_small.py
+++ tiparep2/pupils_small.py
@@ -1,1 +1,24 @@
-#напиши здесь свою программу
+class Pupil():
+    def __init__(self, f, n, p):
+        self.f = f
+        self.n = n
+        self.p = p
+file = open("pupils_txt.txt", "r", encoding='utf-8')
+pupla = file.read().split('\n')
+pupils = []
+for pupil in pupla:
+    pupil = pupil.split(' ')
+    pupils.append(Pupil(pupil[0], pupil[1], pupil[2]))
+s_a = []
+otlichniki = []
+for pupil in pupils:
+    s_a.append(int(pupil.p))
+    print(pupil.f, pupil.n, '-', pupil.p)
+    if pupil.p == '5':
+        otlichniki.append(pupil.f)
+print('Отличники:')
+for i in otlichniki:
+    print(i)
+
+
+print('Средняя оценка класса:', sum(s_a) / len(s_a))
